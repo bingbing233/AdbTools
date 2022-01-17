53c3da5e88af8de61daae89a88c389a28a8c5c59
liangbinghao <liangbinghao@gomo.com>
2022-01-17 11:12:43 +0800

    提交adb三件套

--- src/main/kotlin/MainViewModel.kt
+++ src/main/kotlin/MainViewModel.kt
@@ -4,6 +4,10 @@ class MainViewModel {
 
     var resultStr = MutableStateFlow("命令输出区")
 
+    fun execute(adbCmd:String){
+        resultStr.value = AdbTools.execute(adbCmd)
+    }
+
     fun reboot(){
         resultStr.value = AdbTools.reboot()
     }
@@ -16,7 +20,17 @@ class MainViewModel {
         resultStr.value = AdbTools.getCurrentActivity()
     }
 
-    fun execute(adbCmd:String){
-        resultStr.value = AdbTools.execute(adbCmd)
+    fun getDevices(){
+        resultStr.value = AdbTools.getDevices()
+    }
+
+    fun getConnectionState(){
+        val str = AdbTools.getConnectionState()
+        if (str.contains("device"))
+            resultStr.value = "device --> 设备连接正常"
+        if(str.contains("offline"))
+            resultStr.value = "offline --> 设备离线"
+        if(str.contains("unknown"))
+            resultStr.value = "unknown --> 无设备"
     }
 }
